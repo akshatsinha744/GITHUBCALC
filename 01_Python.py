fd497a23f87c4ce0b3b1be9ccdbaccf1e10abc24
akshatsinha744 <96653938+akshatsinha744@users.noreply.github.com>
2021-12-25 18:32:47 +0530

    Add files via upload

---
+++ 01_Python.py
@@ -0,0 +1,5 @@
+#Author:Akshat(comment is given by using # symbol) '''single line comment'''
+import os 
+'''These are modules there are two types of modules built in and external modules
+external are downloaded by using pip install modulename''' '''multi line comment'''
+print('akshat')
